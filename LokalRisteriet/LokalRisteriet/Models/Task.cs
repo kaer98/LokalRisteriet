c39a721b2222b982a7773eef681a5dea8b2b4842
Jasper <jasper@jazper.dk>
2023-04-14 10:15:01 +0200

    Added Task class

---
+++ LokalRisteriet/LokalRisteriet/Models/Task.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LokalRisteriet.Models
+{
+    internal class Task
+    {
+        public string Name { get; private set; }
+        public bool IsDone { get; private set; }
+
+
+        public Task(string name, bool isDone)
+        { 
+            Name = name;
+            IsDone = isDone;
+        }
+    }
+}
