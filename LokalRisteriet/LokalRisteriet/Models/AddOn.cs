3ac558a25458ac4cd579f83408a70d464a6416b1
Jasper <jasper@jazper.dk>
2023-04-14 10:17:23 +0200

    Add AddOn class

---
+++ LokalRisteriet/LokalRisteriet/Models/AddOn.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LokalRisteriet.Models
+{
+    internal class AddOn
+    {
+        public string Name { get; private set; }
+        public double Price { get; private set; }
+
+        public AddOn(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+    }
+}
