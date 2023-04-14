490cc9ceeb716b8440664dc50289566876e544db
Jasper <jasper@jazper.dk>
2023-04-14 10:35:56 +0200

    Add employee name property

--- LokalRisteriet/LokalRisteriet/Models/Task.cs
+++ LokalRisteriet/LokalRisteriet/Models/Task.cs
@@ -4,13 +4,14 @@ namespace LokalRisteriet.Models
     internal class Task
     {
         public string Name { get; private set; }
-        public bool IsDone { get; private set; }
+        public bool IsDone { get; set; }
+        public string EmployeeName { get; set; }
 
-
-        public Task(string name, bool isDone)
+        public Task(string name, bool isDone, string employeeName)
         { 
             Name = name;
             IsDone = isDone;
+            EmployeeName = employeeName;
         }
     }
 }
