020e03a4307258590ac7bc4b1dd1122ed4834e35
SeymenNW <seymennw@gmail.com>
2023-04-24 11:48:21 +0200

    Updates ViewBooking UI.

--- LokalRisteriet/LokalRisteriet/Views/BookingInfoView.axaml.cs
+++ LokalRisteriet/LokalRisteriet/Views/BookingInfoView.axaml.cs
@@ -1,5 +1,8 @@
+using System;
 using Avalonia.Controls;
 using Avalonia.Interactivity;
+using Avalonia.Layout;
+using Avalonia.Media;
 using LokalRisteriet.ViewModels;
 
 namespace LokalRisteriet.Views
@@ -13,6 +16,35 @@ namespace LokalRisteriet.Views
             DataContext = bookingInfoViewVM;
         }
 
-    
+
+        private void Popup_OnClosed(object? sender, EventArgs e)
+        {
+            
+        }
+
+        private void Popup_OnOpened(object? sender, EventArgs e)
+        {
+            
+        }
+
+        private void AddTasksButton_OnClick(object? sender, RoutedEventArgs e)
+        {
+            
+            // var popupContent = new Grid()
+            // {
+            //     Background = Brushes.White,
+            //     Width = 300,
+            //     Height = 200,
+            // };
+            //
+            // popupContent.Children.Add(new TextBlock()
+            // {
+            //     Text = "Popup content",
+            //     HorizontalAlignment = HorizontalAlignment.Center,
+            //     VerticalAlignment = VerticalAlignment.Center,
+            // });
+            // info_Popup.Child = popupContent;
+            
+        }
     }
 }
