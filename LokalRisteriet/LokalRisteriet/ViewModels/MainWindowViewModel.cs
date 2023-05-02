eab08fd7892bab9d6205c0b249a87227f3b356f0
SeymenNW <seymennw@gmail.com>
2023-05-02 11:22:14 +0200

    ching Chong

--- LokalRisteriet/LokalRisteriet/ViewModels/MainWindowViewModel.cs
+++ LokalRisteriet/LokalRisteriet/ViewModels/MainWindowViewModel.cs
@@ -2,6 +2,12 @@
 {
     public class MainWindowViewModel : ViewModelBase
     {
-        public string Greeting => "Welcome to Avalonia!";
+        //Denne ViewModel bliver ikke brugt. Vi har taget udgangspunkt i Code-behinded
+        // og derfor har det ikke været nødvendigt for at bruge denne ViewModel.
+        
+        /*
+         * Grunden til vi ikke har slettet den er fordi Avalonia UI (Som vi bruger fremfor WPF)
+         * Har brug for at den eksisterer for at kunne fungere. 
+        */
     }
 }
